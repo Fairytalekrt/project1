cdf328a1b6f342f57b174af538a968c9c6725b26
klrwty@gmail.com <klrwty@gmailcom>
2019-08-15 14:34:12 +0800

    Dashboard

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,117 +1,116 @@
 <template>
-  <div>
-      <el-row :gutter="20">
-        <el-col :span="8">
-          <el-card shadow="hover" class="mgb20" style="height:252px;">
-            <div class="user-info">
-              <img src="../../assets/img/img.png" class="user-avator">
-              <div class="user-info-cont">
-                <div class="user-info-name">{{name}}</div>
-                <div>{{role}}</div>
-              </div>
-            </div>
-            <div class="user-info-list">上次登录时间：<span>2018-12-31</span></div>
-            <div class="user-info-list">上次登录地点：<span>珠海</span></div>
-          </el-card>
-          <el-card shadow="hover" style="height:252px;">
-            <div slot="header" class="clearfix">
-              <span>语言详情</span>
-              </div>
-              Vue
-              <el-progress :percentage="71.3" color="#42b983"></el-progress>
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="8">
+                <el-card shadow="hover" class="mgb20" style="height:252px;">
+                    <div class="user-info">
+                        <img src="../../assets/img/img.png" class="user-avator">
+                        <div class="user-info-cont">
+                            <div class="user-info-name">{{name}}</div>
+                            <div>{{role}}</div>
+                        </div>
+                    </div>
+                    <div class="user-info-list">上次登录时间：<span>2018-12-31</span></div>
+                    <div class="user-info-list">上次登录地点：<span>珠海</span></div>
+                </el-card>
+                <el-card shadow="hover" style="height:252px;">
+                    <div slot="header" class="clearfix">
+                        <span>语言详情</span>
+                    </div>
+                    Vue
+                    <el-progress :percentage="71.3" color="#42b983"></el-progress>
                     JavaScript
                     <el-progress :percentage="24.1" color="#f1e05a"></el-progress>
                     CSS
                     <el-progress :percentage="3.7"></el-progress>
                     HTML
                     <el-progress :percentage="0.9" color="#f56c6c"></el-progress>
-               </el-card>
-        </el-col>
-        <el-col :span="16">
-            <el-row :gutter="20" class="mgb20" >
-                <el-col :span="8">
-                  <el-card shadow="hover" :body-style="{padding:'0px'}">
-                    <div class="grid-content grid-con-1"> 
-                      <i class="el-icon-lx-people grid-con-icon"></i>
-                      <div class="grid-cont-right">
-                        <div class="grid-num">3333</div>
-                        <div>用户访问量</div>
-                      </div>
+                </el-card>
+            </el-col>
+            <el-col :span="16">
+                <el-row :gutter="20" class="mgb20">
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-1">
+                                <i class="el-icon-lx-people grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">3333</div>
+                                    <div>用户访问量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-2">
+                                <i class="el-icon-lx-notice grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">233</div>
+                                    <div>系统消息</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{padding: '0px'}">
+                            <div class="grid-content grid-con-3">
+                                <i class="el-icon-lx-goods grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">3000</div>
+                                    <div>数量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <el-card shadow="hover" style="height:403px;">
+                    <div slot="header" class="clearfix">
+                        <span>待办事项</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
                     </div>
-                  </el-card>
-                </el-col>
-                <el-col :span="8">
-                  <el-card shadow="hover" :body-style="{padding :'0px'}">
-                      <div class="grid-content grid-con-2">
-                          <i class="el-icon-lx-notice grid-con-icon"></i>
-                          <div class="grid-cont-right">
-                              <div class="grid-num">233</div>
-                              <div>系统消息</div>
-                          </div>
-                      </div>
-                  </el-card>
-                </el-col>
-                <el-col :span="8">
-                  <el-card shadow="hover" :body-style="{padding :'0px'}">
-                      <div class="grid-content grid-con-3">
-                          <i class="el-icon-lx-goods grid-con-icon"></i>
-                          <div class="grid-cont-right">
-                              <div class="grid-num">3000</div>
-                              <div>数量</div>
-                          </div>
-                      </div>
-                  </el-card>
-                </el-col>
-            </el-row>
-            <el-card shadow="hover" style="height:403px;">
-                <div slot="header" class="clearfix">
-                    <span>待办事项</span>
-                    <el-button style="float:right;padding:3px 0" type="text">添加</el-button>
-                </div>
-                <el-table :data="todoList" :show-header="false" height="304"
-                          style="width: 100%;font-size:14px;">
-                  <el-table-column width="40"> 
-                      <template slot-scope="scope">
-                          <el-checkbox v-model="scope.row.status"></el-checkbox>
-                      </template>
-                  </el-table-column>
-                  <el-table-column>
-                      <template>
-                          <div class="todo-item" :class="{'todo-item-del':scope.row.status}">
-                              {{scope.row.title}}</div>
-                      </template>
-                  </el-table-column>
-                  <el-table-column width="60">
-                      <template slot-scope="scope">
-                          <i class="el-icon-edit"></i>
-                          <i class="el-icon-delete"></i>
-                      </template>
-                  </el-table-column>
-                </el-table>
-            </el-card>
-        </el-col>
-      </el-row>
-        <el-row :gutter="20" >
+                    <el-table :data="todoList" :show-header="false" height="304" style="width: 100%;font-size:14px;">
+                        <el-table-column width="40">
+                            <template slot-scope="scope">
+                                <el-checkbox v-model="scope.row.status"></el-checkbox>
+                            </template>
+                        </el-table-column>
+                        <el-table-column>
+                            <template slot-scope="scope">
+                                <div class="todo-item" :class="{'todo-item-del': scope.row.status}">{{scope.row.title}}</div>
+                            </template>
+                        </el-table-column>
+                        <el-table-column width="60">
+                            <template>
+                                <i class="el-icon-edit"></i>
+                                <i class="el-icon-delete"></i>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-row :gutter="20">
             <el-col :span="12">
                 <el-card shadow="hover">
                     <schart ref="bar" class="schart" canvasId="bar" :data="data" type="bar" :options="options"></schart>
                 </el-card>
             </el-col>
             <el-col :span="12">
-                <el-card>
+                <el-card shadow="hover">
                     <schart ref="line" class="schart" canvasId="line" :data="data" type="line" :options="options2"></schart>
                 </el-card>
             </el-col>
         </el-row>
-  </div>
+    </div>
 </template>
+
 <script>
-    import Schart from "vue-schart";
-    import bus from '../common/bus'
+    import Schart from 'vue-schart';
+    import bus from '../common/bus';
     export default {
-      name:'dashboard',
-      data(){
-        return {
+        name: 'dashboard',
+        data() {
+            return {
                 name: localStorage.getItem('ms_username'),
                 todoList: [{
                         title: '今天要修复100个bug',
@@ -183,17 +182,54 @@
                     topPadding: 30
                 }
             }
-      },
-      components:{
-        Schart
-      },
-      computed: {
+        },
+        components: {
+            Schart
+        },
+        computed: {
             role() {
                 return this.name === 'admin' ? '超级管理员' : '普通用户';
             }
-      },
-    };
+        },
+        created(){
+            this.handleListener();
+            this.changeDate();
+        },
+        activated(){
+            this.handleListener();
+        },
+        deactivated(){
+            window.removeEventListener('resize', this.renderChart);
+            bus.$off('collapse', this.handleBus);
+        },
+        methods: {
+            changeDate(){
+                const now = new Date().getTime();
+                this.data.forEach((item, index) => {
+                    const date = new Date(now - (6 - index) * 86400000);
+                    item.name = `${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()}`
+                })
+            },
+            handleListener(){
+                bus.$on('collapse', this.handleBus);
+                // 调用renderChart方法对图表进行重新渲染
+                window.addEventListener('resize', this.renderChart)
+            },
+            handleBus(msg){
+                setTimeout(() => {
+                    this.renderChart()
+                }, 300);
+            },
+            renderChart(){
+                this.$refs.bar.renderChart();
+                this.$refs.line.renderChart();
+            }
+        }
+    }
+
 </script>
+
+
 <style scoped>
     .el-row {
         margin-bottom: 20px;
